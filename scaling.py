eaeea573408807842435d58c9a358c7cd82801b0
Tyler Tripp <tripptd@lateralus.cs.appstate.edu>
2023-11-30 10:16:51 -0500

    move test to windows

--- scaling.py
+++ scaling.py
@@ -1,11 +1,14 @@
 import numpy as np
 import os
+import plotly.graph_objects as go
+import plotly.io as pio
 from sklearn.preprocessing import RobustScaler
 import random
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.figure_factory as ff
 
 def get_timbre_opts():
     opts = os.listdir('./logs/')
@@ -94,9 +97,13 @@ def make_graph_histograms(df_top):
 
 def get_robust_scaled_data():
     arr = load_scaling_data()
+    #print("original arr")
+    #print(arr)
     transformer = RobustScaler().fit(arr)
     scaled_arr = transformer.transform(arr)
-    return transformer, scaled_arr
+    #print("scaled arr")
+    #print(scaled_arr)
+    return transformer, scaled_arr, arr
 
 def save_index_vecs(num_indexes):
     index_arr = np.empty((num_indexes,12), np.float32)
@@ -110,10 +117,13 @@ def save_index_vecs(num_indexes):
     for idx, feature in enumerate(features):
         uniform_min = np.min(feature)
         uniform_max = np.max(feature)
-        print(np.min(feature), ' ', np.max(feature))
+        print(f'min, max of feature {idx}', np.min(feature), ' ', np.max(feature))
         samples = np.random.default_rng().uniform(uniform_min, uniform_max, num_indexes)
         index_arr[:, idx] = samples
-    np.save(index_vecs_fp + f'{num_indexes}_robust_float32_indexes', index_arr)
+        print('sample selected: ', samples)
+    index_arr = np.load(index_vecs_fp + f'{num_indexes}_robust_float32_indexes.npy')
+    #print(index_arr)
+    #np.save(index_vecs_fp + f'{num_indexes}_robust_float32_indexes', index_arr)
         #density, bin_edges = np.histogram(feature, bins=100, density=True)
         #distributions.append(density)
         #bin_edges_list.append(bin_edges)
@@ -122,7 +132,6 @@ def save_index_vecs(num_indexes):
         #print('bin edges:')
         #print(bin_edges)
     #print(distributions)
-    #np.random.choice(training_data)
     #np.save('./index_vecs/64_index_robust_scaled_float32', index_arr)
 
 def plotting_sns_hists():
@@ -139,8 +148,98 @@ def plotting_sns_hists():
         plt.clf()
         idx+=1
 
+def get_index_vec_trace(index_vec):
+    # Add a 3D vector using go.Scatter3d
+    vec_trace = go.Scatter3d(
+        x=[0, index_vec[0]],
+        y=[0, index_vec[1]],
+        z=[0, index_vec[2]],
+        mode='lines+markers',
+        line=dict(color='blue', width=4),
+        marker=dict(color='blue', size=8),
+        name='Vector',
+        showlegend=False
+    )
+    return vec_trace
+
+def plotting():
+    transformer, scaled_data, data = get_robust_scaled_data()
+    #print(scaled_data.shape)
+    #print(data.shape)
+
+    scaled = pd.DataFrame(scaled_data)
+    #original = pd.DataFrame(data)
+    #col_names = ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12']
+    scol_names = ['scaled_t1', 'scaled_t2', 'scaled_t3', 'scaled_t4', 'scaled_t5', 'scaled_t6', 'scaled_t7', 'scaled_t8', 'scaled_t9', 'scaled_t10', 'scaled_t11', 'scaled_t12']
+    scaled.columns = scol_names
+    #original.columns = col_names
+
+    #concat_df = pd.concat([original, scaled], axis=1)
+    index_arr = np.load('./index_vecs/'+ f'64_robust_float32_indexes.npy')
+    #fig = px.histogram(scaled.head(10000), x=scol_names, marginal='rug', nbins=100, histnorm='probability density') 
+    #pio.write_html(fig, 'original_histograms.html')
+    #pio.write_html(fig, 'scaled_histograms.html') 
+    """GET 3d ORIGNal
+    original = original.head(10000)
+    original['avg'] = (original["t1"] + original["t2"] + original["t3"]) / 3
+    fig = px.scatter_3d(original[["t1", "t2", "t3", "avg"]], x='t1', y='t2', z='t3',
+              color=original['avg'])
+    
+    fig.update_layout(scene=dict(xaxis=dict(range=[original["t1"].min(), original["t1"].max()]), yaxis=dict(range=[original["t2"].min(), original["t2"].max()]), zaxis=dict(range=[original["t3"].min(), original["t3"].max()])))
+
+    pio.write_html(fig, '3d_original.html')
+    """
+    scaled = scaled.head(10000)
+    scaled['avg'] = (scaled["scaled_t1"] + scaled["scaled_t2"] + scaled["scaled_t3"]) / 3
+    fig = go.Figure()
+    #fig = px.scatter_3d(scaled[["scaled_t1", "scaled_t2", "scaled_t3", "avg"]], x='scaled_t1', y='scaled_t2', z='scaled_t3', color=scaled['avg'], showlegend=False)
+    scatter_trace = go.Scatter3d(
+        x=scaled["scaled_t1"],
+        y=scaled["scaled_t2"],
+        z=scaled["scaled_t3"],
+        mode="markers",
+        marker=dict(
+            size=7,
+            color=scaled["avg"],
+            colorscale="Viridis",
+            opacity=1
+        ),
+        showlegend=False
+    )
+    fig.add_trace(scatter_trace)
+    #Make the grid scale better.
+    #fig.update_layout(scene=dict(xaxis=dict(range=[scaled["scaled_t1"].min(), scaled["scaled_t1"].max()]), yaxis=dict(range=[scaled["scaled_t2"].min(), scaled["scaled_t2"].max()]), zaxis=dict(range=[scaled["scaled_t3"].min(), scaled["scaled_t3"].max()])))
+    
+    #Same grid for surface making.
+    plane_x = np.linspace(scaled["scaled_t1"].min(), scaled["scaled_t1"].max(), 100)
+    plane_y = np.linspace(scaled["scaled_t2"].min(), scaled["scaled_t2"].max(), 100)
+    plane_x, plane_y = np.meshgrid(plane_x, plane_y)
+    #print(index_arr[0,0:3])
+    #print(np.array([scaled.iloc[0,0], scaled.iloc[0,1], scaled.iloc[0,2]]))
+    #plane_surface = get_cosine_sim_area(index_arr[0,0:3])
+    #neg_surf, pos_surf = cosine_negative(index_arr[0,0:3], plane_x, plane_y)
+    
+    for index_vec in index_arr[0:4,:]:
+        a,b,c = index_vec[0:3]
+        plane_z = (-a * plane_x - b * plane_y) / c
+        surf = go.Surface(x=plane_x, y=plane_y, z=plane_z, opacity=0.5, showlegend=False)
+        fig.add_trace(surf)
+        vec = get_index_vec_trace(index_vec[0:3])
+        fig.add_trace(vec)
+    #fig.add_trace(neg_surf)
+    #fig.add_trace(pos_surf)
+    fig.update_layout(showlegend=False, scene=dict(xaxis=dict(range=[scaled["scaled_t1"].min(), scaled["scaled_t1"].max()]), yaxis=dict(range=[scaled["scaled_t2"].min(), scaled["scaled_t2"].max()]), zaxis=dict(range=[scaled["scaled_t3"].min(), scaled["scaled_t3"].max()])))
+    #12 values
+    #fig.update_traces(marker_showscale=False)
+    pio.write_html(fig, '3d_sliced.html')
+"""
+    #pio.write_html(fig, '3d_scaled.html')
+"""
 def main():
-   save_index_vecs(64)
+   plotting()
+   #plotting_sns_hists()
+   #save_index_vecs(64)
+   return
 
 if __name__ == "__main__":
     main()
